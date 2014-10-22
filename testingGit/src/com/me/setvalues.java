4d9c455b19f01b38b664b4ef1f13d13094789403
ankit <ankit.patel@kiwiqa.com>
2014-10-22 11:11:10 +0530

    First Commit

---
+++ testingGit/src/com/me/setvalues.java
@@ -0,0 +1,16 @@
+package com.me;
+
+
+public class setvalues {
+
+	private int emailcount=0;
+
+	public int getEmailcount() {
+		return emailcount;
+	}
+
+	public void setEmailcount(int emailcount) {
+		this.emailcount = emailcount;
+	}
+	
+}
